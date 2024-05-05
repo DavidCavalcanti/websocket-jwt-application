0acbed16b91d1a1447614098e26f5c28aab0feb2
David Cavalcanti <david_cavalcanti@outlook.com>
2024-05-05 01:31:15 -0300

    feature/socket - create socket conection

--- src/servidor.js
+++ src/servidor.js
@@ -1,14 +1,25 @@
 import express from "express";
+import http from "http";
 import path from "path";
+import { Server } from "socket.io";
 import url from "url";
 
 const app = express();
+const servidorHttp = http.createServer(app);
+const io = new Server(servidorHttp);
+
 const porta = process.env.PORT || 3000;
 
 const caminhoAtual = url.fileURLToPath(import.meta.url);
 const diretorioPublic = path.join(caminhoAtual, "../..", "public");
+
 app.use(express.static(diretorioPublic));
 
-app.listen(porta, () => {
+io.on('connection', (Socket) => {
+    console.log("Um cliente se conectou!");
+})
+
+servidorHttp.listen(porta, () => {
     console.log(`servidor escutando em http://localhost:${porta}`);
 });
+
