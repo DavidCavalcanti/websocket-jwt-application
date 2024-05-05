9c71956da2c23a865ca12229714efcf2e9be8c50
David Cavalcanti <david_cavalcanti@outlook.com>
2024-05-05 00:09:21 -0300

    using static files

--- src/servidor.js
+++ src/servidor.js
@@ -1,8 +1,13 @@
 import express from "express";
+import path from "path";
+import url from "url";
 
 const app = express();
-
 const porta = process.env.PORT || 3000;
+
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+const diretorioPublic = path.join(caminhoAtual, "../..", "public");
+app.use(express.static(diretorioPublic));
 
 app.listen(porta, () => {
     console.log(`servidor escutando em http://localhost:${porta}`);
