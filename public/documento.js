afd829dff1d4a15d90550c30416672a9fe199cb1
David Cavalcanti <david_cavalcanti@outlook.com>
2024-05-06 20:22:33 -0300

    feature/socket emit front-end event socket

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,7 @@
 const socket = io();
+
+const textEditor = document.getElementById("editor-texto");
+
+textEditor.addEventListener("keyup", () => {
+    socket.emit("texto_editor", textEditor.value);
+});
